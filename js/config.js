d13107ed8f1040f98bcd9d20728801a6aa3f975a
abdu07aa-cmyk <abdu07.aa@gmail.com>
2026-07-30 10:32:25 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // js/config.js
 const SUPABASE_CONFIG = {
-    url: 'https://your-project-id.supabase.co',
-    anonKey: 'your-anon-key-here'
+    url: 'https://https://cklenalougtlblexnguk.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrbGVuYWxvdWd0bGJsZXhuZ3VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzNzE3MzEsImV4cCI6MjEwMDk0NzczMX0.Hne4XhUqXAq1xbTDqtRus4QDU4xolbISUJLbFLZh1bw'
 };
 
 // Untuk production, gunakan environment variables

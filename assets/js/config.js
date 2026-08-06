4f7af2c453ffe7caf55e003383ac59668280651f
abdu07aa-cmyk <abdu07.aa@gmail.com>
2026-08-06 13:58:29 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,5 +2,5 @@
 // ISI DENGAN DATA SUPABASE PROJECT KAMU
 // Ambil dari: Supabase Dashboard → Project Settings → API
 // =====================================================================
-const SUPABASE_URL = "https://YOUR-PROJECT-REF.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR-ANON-PUBLIC-KEY";
+const SUPABASE_URL = "https://rdzrzzjykhbqvaaxdrvq.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkenJ6emp5a2hicXZhYXhkcnZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU5NTU3MTEsImV4cCI6MjEwMTUzMTcxMX0.sF3NqFM6ldqEAsd0Mq_wKUvfkoACAlohM5VW92zhcKg";
